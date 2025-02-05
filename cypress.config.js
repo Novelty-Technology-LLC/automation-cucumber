0b11fa455d92d99df3a77c5252aa584538ff8797
deepsanathapa <deepsana@noveltytechnology.com>
2025-02-05 15:12:35 +0545

    report packages

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,22 @@
 const { defineConfig } = require('cypress');
 const dotenv = require('dotenv').config();
 const cucumber = require('cypress-cucumber-preprocessor').default;
+const mochawesome = require('cypress-mochawesome-reporter/plugin');
 
 module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/feature/*.feature', // Recognizes feature files
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber()); // Enables Cucumber preprocessor
+      mochawesome(on); // Enables Mochawesome reporter
       return config;
+    },
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: true,
+      html: true,
+      json: true,
     },
     env: {
       BASE_URL: process.env.CYPRESS_BASE_URL,
