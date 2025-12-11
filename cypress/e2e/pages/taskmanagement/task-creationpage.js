0f55c59fca351d4c8da23ae55e8cfd203cf29a89
deepsanathapa <deepsana@noveltytechnology.com>
2025-12-11 18:20:50 +0545

    added scenarios for task creation for queue

--- cypress/e2e/pages/taskmanagement/task-creationpage.js
+++ cypress/e2e/pages/taskmanagement/task-creationpage.js
@@ -26,17 +26,102 @@ class TaskCreationPage {
   }
 
   uploadIBAndIBDFiles() {
-    this.uploadFileToSftp('cypress/fixtures/3395969(IB-IBD).xlsx', '/qa/ardb/3395969(IB-IBD).xlsx');
+    this.uploadFileToSftp('cypress/fixtures/3452772(IB-IBD).xlsx', '/qa/ardb/3452772(IB-IBD).xlsx');
     cy.wait(20000);
   }
 
   upload835EDIFile() {
-    this.uploadFileToSftp('cypress/fixtures/3395969.DAT', '/qa/edi_835/3395969.DAT');
+    this.uploadFileToSftp('cypress/fixtures/3452772.edi', '/qa/edi_835/3452772.edi');
+    cy.wait(30000);
+  }
+
+  uploadIBAndIBDFilesForCommercial() {
+    this.uploadFileToSftp('cypress/fixtures/commercial/3452772(IB-IBD).xlsx', '/qa/ardb/3452772(IB-IBD).xlsx');
+    cy.wait(20000);
+  }
+
+  upload835EDIFileForCommercial() {
+    this.uploadFileToSftp('cypress/fixtures/commercial/3452772.edi', '/qa/edi_835/3452772.edi');
+    cy.wait(30000);
+  }
+
+  uploadIBAndIBDFilesForSignature() {
+    this.uploadFileToSftp('cypress/fixtures/signature/3395358(IB-IBD).xlsx', '/qa/ardb/3395969(IB-IBD).xlsx');
+    cy.wait(20000);
+  }
+
+  upload835EDIFileForSignature() {
+    this.uploadFileToSftp('cypress/fixtures/signature/3395358(partial).edi', '/qa/edi_835/3395358(partial).edi');
+    cy.wait(30000);
+    this.uploadFileToSftp('cypress/fixtures/signature/3395358(partial2).edi', '/qa/edi_835/3395358(partial2).edi');
     cy.wait(30000);
   }
 
   verifyInvoiceBillingTaskCreated() {
     cy.get('[data-cy="queue-header"]').should('contain', 'Commercial');
+  }
+
+  verifyTaskCreatedInCommercialQueue() {
+    cy.get('[data-cy="queue-header"]', { timeout: 30000 })
+      .contains('Commercial')
+      .should('be.visible');
+  }
+
+  verifyTaskCreatedInSignatureQueue() {
+    cy.get('[data-cy="queue-header"]', { timeout: 30000 })
+      .contains('Signature')
+      .should('be.visible');
+  }
+
+  uploadIBAndIBDFilesForAHF() {
+    this.uploadFileToSftp('cypress/fixtures/ahf/3394368(IB-IBD).xlsx', '/qa/ardb/3394368(IB-IBD).xlsx');
+    cy.wait(20000);
+  }
+
+  upload835EDIFileForAHF() {
+    this.uploadFileToSftp('cypress/fixtures/ahf/3394368.edi', '/qa/edi_835/3394368.edi');
+    cy.wait(30000);
+  }
+
+  uploadIBAndIBDFilesForMedicare() {
+    this.uploadFileToSftp('cypress/fixtures/medicare/3453300(IB-IBD).xlsx', '/qa/ardb/3453300(IB-IBD).xlsx');
+    cy.wait(20000);
+  }
+
+  upload835EDIFileForMedicare() {
+    this.uploadFileToSftp('cypress/fixtures/medicare/3453300.edi', '/qa/edi_835/3453300.edi');
+    cy.wait(30000);
+  }
+
+  uploadIBAndIBDFilesForMedical() {
+    this.uploadFileToSftp('cypress/fixtures/medical/3314780(IB-IBD).xlsx', '/qa/ardb/3314780(IB-IBD).xlsx');
+    cy.wait(20000);
+  }
+
+  upload835EDIFileForMedical() {
+    this.uploadFileToSftp('cypress/fixtures/medical/3314780(x288).edi', '/qa/edi_835/3314780(x288).edi');
+    cy.wait(30000);
+  }
+
+  verifyTaskCreatedInAHFQueue() {
+    cy.get('[data-cy="queue-header"]', { timeout: 30000 })
+      .contains('AHF')
+      .scrollIntoView()
+      .should('be.visible');
+  }
+
+  verifyTaskCreatedInMedicareQueue() {
+    cy.get('[data-cy="queue-header"]', { timeout: 30000 })
+      .contains('Medicare')
+      .scrollIntoView()
+      .should('be.visible');
+  }
+
+  verifyTaskCreatedInMedicalQueue() {
+    cy.get('[data-cy="queue-header"]', { timeout: 30000 })
+      .contains('Medical')
+      .scrollIntoView()
+      .should('be.visible');
   }
 }
 
